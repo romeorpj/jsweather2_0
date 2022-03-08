e3aa9e984188688c041af95b1f40e7b37cd9ea32
Richard Jackson <rpjackson2@gmail.com>
2022-03-08 03:23:24 -0600

    added fetch request

--- js/app.js
+++ js/app.js
@@ -1,14 +1,41 @@
 "use strict";
+import axios from "axios";
+
 let secondaryPage = document.querySelector(".secondary-page")
 let mainPage = document.querySelector(".main-page");
-let apiKey = ee93106d0f65de4553d6531305b257fe;
+let apiKey = "ee93106d0f65de4553d6531305b257fe";
+
+//going to fetch open weather api
+//use the geo-navigator to get access to lat and long
+let getWeatherData = async() => {
+    try {
+
+        const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}').then(res => res.json())
+            .then((data) => {
+                // ENTER CODE HERE
+            });
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// openwether onecall api
+//
+
+
+
+
 
 //Event listener that slides up the second page
 //this works by adding the slide class that was defined and created in the css
 //whenever the main page button is clicked
 document.querySelector(".main-page-button").addEventListener("click",()=>{
-secondaryPage.classList.add("slide")
-    console.log("GET WEATHER BUTTON CLICKED")
+secondaryPage.classList.add("slide");
+    // console.log("GET WEATHER BUTTON CLICKED")
+
+    // TODO:When main buttom is clicked, run the geolocator to retrieve openweather api data.
+
 })
 
 //event listener that goes back to the mainpage from the second page
@@ -20,9 +47,3 @@ document.querySelector(".btn-to-main").addEventListener("click",()=>{
     console.log("GET WEATHER BUTTON CLICKED go back")
 })
 
-//going to fetch open weather api
-//use the geo-navigator to get access to lat and long
-
-
-//openwether onecall api
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
